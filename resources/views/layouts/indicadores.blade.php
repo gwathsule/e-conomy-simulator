452c2a2e031c2c96a0b671433130a27cb1957932
Rafael dos Santos Gomes <santos@imaginationmedia.com>
2020-05-18 11:19:15 -0300

    trocando grafico

--- resources/views/layouts/indicadores.blade.php
+++ resources/views/layouts/indicadores.blade.php
@@ -1,111 +1,56 @@
 <script>
     window.onload = function () {
+        /* chart.js chart examples */
 
-        var chart = new CanvasJS.Chart("chartContainer", {
-            animationEnabled: true,
-            zoomEnabled: true,
-            theme: "dark2",
-            title:{
-                text: "Business Growth From 2000 to 2017"
-            },
-            axisX:{
-                title: "Year",
-                valueFormatString: "####",
-                interval: 2
-            },
-            axisY:{
-                logarithmic: true, //change it to false
-                title: "Profit in USD (Log)",
-                prefix: "$",
-                titleFontColor: "#6D78AD",
-                lineColor: "#6D78AD",
-                gridThickness: 0,
-                lineThickness: 1,
-                //includeZero: false,
-                labelFormatter: addSymbols
-            },
-            axisY2:{
-                title: "Profit in USD",
-                prefix: "$",
-                titleFontColor: "#51CDA0",
-                logarithmic: false, //change it to true
-                lineColor: "#51CDA0",
-                gridThickness: 0,
-                lineThickness: 1,
-                labelFormatter: addSymbols
-            },
-            legend:{
-                verticalAlign: "top",
-                fontSize: 16,
-                dockInsidePlotArea: true
-            },
-            data: [{
-                type: "line",
-                xValueFormatString: "####",
-                yValueFormatString: "$#,##0.##",
-                showInLegend: true,
-                name: "Log Scale",
-                dataPoints: [
-                    { x: 2001, y: 8000 },
-                    { x: 2002, y: 20000 },
-                    { x: 2003, y: 40000 },
-                    { x: 2004, y: 60000 },
-                    { x: 2005, y: 90000 },
-                    { x: 2006, y: 140000 },
-                    { x: 2007, y: 200000 },
-                    { x: 2008, y: 400000 },
-                    { x: 2009, y: 600000 },
-                    { x: 2010, y: 800000 },
-                    { x: 2011, y: 900000 },
-                    { x: 2012, y: 1400000 },
-                    { x: 2013, y: 2000000 },
-                    { x: 2014, y: 4000000 },
-                    { x: 2015, y: 6000000 },
-                    { x: 2016, y: 8000000 },
-                    { x: 2017, y: 9000000 }
-                ]
-            },
-                {
-                    type: "line",
-                    xValueFormatString: "####",
-                    yValueFormatString: "$#,##0.##",
-                    axisYType: "secondary",
-                    showInLegend: true,
-                    name: "Linear Scale",
-                    dataPoints: [
-                        { x: 2001, y: 8000 },
-                        { x: 2002, y: 20000 },
-                        { x: 2003, y: 40000 },
-                        { x: 2004, y: 60000 },
-                        { x: 2005, y: 90000 },
-                        { x: 2006, y: 140000 },
-                        { x: 2007, y: 200000 },
-                        { x: 2008, y: 400000 },
-                        { x: 2009, y: 600000 },
-                        { x: 2010, y: 800000 },
-                        { x: 2011, y: 900000 },
-                        { x: 2012, y: 1400000 },
-                        { x: 2013, y: 2000000 },
-                        { x: 2014, y: 4000000 },
-                        { x: 2015, y: 6000000 },
-                        { x: 2016, y: 8000000 },
-                        { x: 2017, y: 9000000 }
-                    ]
-                }]
-        });
-        chart.render();
+        // chart colors
+        var colors = ['#007bff','#28a745','#ff0000'];
 
-        function addSymbols(e){
-            var suffixes = ["", "K", "M", "B"];
+        /* large line chart */
+        var chLine = document.getElementById("chLine");
+        var chartData = {
+            labels: ["S", "M", "T", "W", "T", "F", "S"],
+            datasets: [{
+                data: [589, 445, 483, 503, 689, 692, 634],
+                backgroundColor: 'transparent',
+                borderColor: colors[0],
+                borderWidth: 4,
+                pointBackgroundColor: colors[0]
+            },
+            {
+                data: [222, 125, 365, 954, 589, 520, 423],
+                backgroundColor: 'transparent',
+                borderColor: colors[1],
+                borderWidth: 4,
+                pointBackgroundColor: colors[1]
+            },
+            {
+                data: [739, 865, 293, 178, 389, 232, 974],
+                backgroundColor: 'transparent',
+                borderColor: colors[2],
+                borderWidth: 4,
+                pointBackgroundColor: colors[2]
+            }
+            ]
+        };
 
-            var order = Math.max(Math.floor(Math.log(e.value) / Math.log(1000)), 0);
-            if(order > suffixes.length - 1)
-                order = suffixes.length - 1;
-
-            var suffix = suffixes[order];
-            return CanvasJS.formatNumber(e.value / Math.pow(1000, order)) + suffix;
+        if (chLine) {
+            new Chart(chLine, {
+                type: 'line',
+                data: chartData,
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero: false
+                            }
+                        }]
+                    },
+                    legend: {
+                        display: false
+                    }
+                }
+            });
         }
-
     }
 </script>
 
@@ -116,7 +61,7 @@
         </div>
         <div class="card-body" style="height: 400px">
             <div class="row">
-                <div id="chartContainer" class="col-md-9 mr-3" style="height: 370px;"></div>
+                <canvas id="chLine" class="col-md-9 mr-3" style="height: 370px;"></canvas>
                 <div id="legenda" class="col-md-2">
                     <!-- Default unchecked -->
                     <div class="custom-control custom-checkbox">
