daace5b81c7e089f7f824cadd4310af135978947
Rafael dos Santos Gomes <santos@imaginationmedia.com>
2021-03-14 11:08:52 -0300

    adicionando pop ups de noticias

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,24 +6,26 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="http://netdna.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="{{asset('css/estilo.css')}}">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
     @yield('css_adicionais')
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
 </head>
 <body>
-<div class="btn_up"><a href="#"><img src="{{asset('img/resources/up-chevron.svg')}}"></a></div>
-<div class="container-economy">
-    <div class="page-inner no-page-title main-page">
-        <!-- start page main wrapper -->
-        @yield('conteudo')
-        <!-- end page main wrapper -->
-        <div class="page-footer">
-            <p>E-conomy Simulator © 2020.</p>
+    <div class="btn_up"><a href="#"><img src="{{asset('img/resources/up-chevron.svg')}}"></a></div>
+    <div class="container-economy">
+        <div class="page-inner no-page-title main-page">
+            <!-- start page main wrapper -->
+            @yield('conteudo')
+            <!-- end page main wrapper -->
+            <div class="page-footer">
+                <p>E-conomy Simulator © 2020.</p>
+            </div>
         </div>
     </div>
-</div>
-<script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
-<script src="http://netdna.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
-@yield('js_adicionais')
-@include('alerts.errors')
+    <script src="http://code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+    @yield('js_adicionais')
+    @include('alerts.errors')
 </body>
 </html>
