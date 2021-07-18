742ad6cdc5dbe0877544ebdd8688f880ba6a23ad
Rafael dos Santos Gomes <santos@imaginationmedia.com>
2021-07-18 19:59:41 -0300

    instalado o gerador de diagrama de classes

--- app/Domains/Medida/Services/ExcluirMedida.php
+++ app/Domains/Medida/Services/ExcluirMedida.php
@@ -7,4 +7,13 @@ use App\Support\Service;
 class ExcluirMedida extends Service
 {
 
+    public function validate(array $data)
+    {
+        // TODO: Implement validate() method.
+    }
+
+    protected function perform(array $data, array $columns = ['*'], array $relations = [])
+    {
+        // TODO: Implement perform() method.
+    }
 }
